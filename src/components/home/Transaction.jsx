887982b36ab1cf760a3b71fd3fbe43304ab6a08e
Nitzan <prnitzan@gmail.com>
2024-02-22 12:00:55 +0200

    Perform minor cleanup

--- src/components/home/Transaction.jsx
+++ src/components/home/Transaction.jsx
@@ -21,15 +21,12 @@ export default function Transaction({ transaction, handleRemoveTransaction }) {
         {`₪${transaction.sum}`}
       </div>
       <div className="transaction-date">{formattedDate}</div>
-      {/* remove btn for dev needs: */}
-      {/* <div className="remove-transaction-btn-container"> */}
         <button
           className="remove-transaction-btn"
           onClick={() => handleRemoveTransaction(transaction.id)}
         >
           x
         </button>
-      {/* </div> */}
     </li>
   );
 }
