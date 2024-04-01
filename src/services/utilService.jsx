ebf93d5a308000fcae6c34db0e0f75f00cbf4cce
Nitzan <prnitzan@gmail.com>
2024-04-01 12:39:29 +0300

    Fix a bug in getLastMonthProperties function which makes getting the correct lastMonth unreliable

--- src/services/utilService.jsx
+++ src/services/utilService.jsx
@@ -39,15 +39,27 @@ export function compareDates(a, b) {
 
 export function getLastMonthProperties() {
   const currentDate = new Date();
-  currentDate.setMonth(currentDate.getMonth() - 1);
-  const lastMonthsYear = currentDate.getFullYear();
-  const lastMonth = currentDate.getMonth();
+  let lastMonth = currentDate.getMonth(); // Zero-based index which means 0 is Jan, 1 is Feb etc
+
+  // Adjust the month if the current month is January
+  if (lastMonth === 0) {
+    lastMonth = 11; // December of the previous year
+  } else {
+    lastMonth -= 1; // Get the previous month
+  }
+
+  // If last month was Dec of last year then we reduce 1 from current year in order to get last year
+  const lastMonthsYear = lastMonth === 11 ? currentDate.getFullYear() - 1 : currentDate.getFullYear();
+
+  // Add 1 to lastMonth to convert zero-based index to one-based index
+  lastMonth += 1;
 
   return { lastMonthsYear, lastMonth };
 }
 
 export function getCurrentMonth() {
-  const currYearStr = new Date().getFullYear().toString();
-  const currMonthStr = (new Date().getMonth() + 1).toString().padStart(2, "0");
-  return `${currYearStr}-${currMonthStr}`;
+  const currDate = new Date();
+  const currYear = currDate.getFullYear();
+  const currMonth = (currDate.getMonth() + 1).toString().padStart(2, "0");
+  return `${currYear}-${currMonth}`;
 }
